cc4b4ed4f838288282630147e91c23ab723b2cbc
DungHo <dung.hoduy@asnet.com.vn>
2017-11-15 15:17:35 +0700

    Add voted component

---
+++ angular2-com/src/app/voted/voted.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-voted',
+  templateUrl: './voted.component.html',
+  styleUrls: ['./voted.component.css']
+})
+export class VotedComponent implements OnInit {
+  @Input() author: string;
+  @Output() onVoted = new EventEmitter<boolean>()
+  voted = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  vote(agree: boolean) {
+    this.voted = true;
+    this.onVoted.emit(agree);
+  }
+
+  changeAuthor(author) {
+    this.author = author;
+  }
+}
