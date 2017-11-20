903e36348318162494fb0dabfa6dc6e175594c9d
DungHo <dung.hoduy@asnet.com.vn>
2017-11-20 13:56:38 +0700

    ng-content demo

--- angular2-com/src/app/app.component.ts
+++ angular2-com/src/app/app.component.ts
@@ -1,20 +1,34 @@
-import { Component, ViewChild } from '@angular/core';
+import { IpService } from './ip.service';
+import { AlertComponent } from './alert/alert.component';
+import { Component, ViewChild, ViewChildren, QueryList, AfterViewInit, ElementRef, ContentChild, OnInit } from '@angular/core';
 import { VotedComponent } from './voted/voted.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: []
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+
   title = 'First App Angular2';
   agree = 0;
+  ip: string;
   disgree = 0;
   authors = ['Ms A', 'Ms B', 'Ms C'];
   dataName = '';
+  @ViewChildren(AlertComponent, {read: ElementRef}) alers: QueryList<AlertComponent>;
   @ViewChild(VotedComponent)
   private voteComponent: VotedComponent;
 
+  constructor(private ipService: IpService) {}
+  ngAfterViewInit() {
+    this.alers.forEach(alerInstance => console.log(alerInstance));
+  }
+  ngOnInit(): void {
+    this.ipService.getIp()
+    .then(ip => this.ip = ip);
+  }
   counterVoted(agree) {
     if (agree) {
       this.agree++;
--- angular2-com/src/app/app.module.ts
+++ angular2-com/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { IpService } from './ip.service';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,6 +9,8 @@ import { CityComponent } from './city/city.component';
 import { TransclusionComponent } from './transclusion/transclusion.component';
 import { CountryComponent } from './country/country.component';
 import { SpecialSelectorComponent } from './special-selector/special-selector.component';
+import { AlertComponent } from './alert/alert.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -16,13 +19,15 @@ import { SpecialSelectorComponent } from './special-selector/special-selector.co
     CityComponent,
     TransclusionComponent,
     CountryComponent,
-    SpecialSelectorComponent
+    SpecialSelectorComponent,
+    AlertComponent
 ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [IpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
