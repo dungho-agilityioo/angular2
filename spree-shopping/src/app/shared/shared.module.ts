a706386c0b632dbb7cca75d666e3d93ebb8116fd
DungHo <dung.hoduy@asnet.com.vn>
2017-12-07 17:32:30 +0700

    [#3] Rename share to shared module

--- spree-shopping/src/app/shared/shared.module.ts
+++ spree-shopping/src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShareComponent } from './share.component';
+import { SharedComponent } from './shared.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ShareComponent]
+  declarations: [SharedComponent]
 })
-export class ShareModule { }
+export class SharedModule { }
