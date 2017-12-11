aa64f0782264bc01bfffd3ef47c2ac3c3ab1e3f1
DungHo <dung.hoduy@asnet.com.vn>
2017-12-11 17:15:40 +0700

    [#21] Update shared module

--- spree-shopping/src/app/app.module.ts
+++ spree-shopping/src/app/app.module.ts
@@ -1,15 +1,13 @@
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { RouterModule } from '@angular/router';
-import { AppRoutes } from './app.routing';
-import { ProductRoutes } from './product/product.routing';
 import { ProductModule } from './product/product.module';
-import { HttpService } from './core/services/http.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpModule, Http } from '@angular/http';
+
 
 @NgModule({
   declarations: [
@@ -18,11 +16,9 @@ import { HttpModule, Http } from '@angular/http';
   imports: [
     BrowserModule,
     ProductModule,
-    ProductRoutes,
     CoreModule,
     SharedModule,
-    AppRoutes,
-    RouterModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
