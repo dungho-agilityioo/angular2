abaebcb3e4a30cca7c09b04436353415f09fb993
DungHo <dung.hoduy@asnet.com.vn>
2017-12-13 18:23:41 +0700

    [#8] Display category on view

--- spree-shopping/src/app/category/category.component.ts
+++ spree-shopping/src/app/category/category.component.ts
@@ -1,3 +1,5 @@
+import { Taxon } from './../product/models/taxon.model';
+import { CategoryService } from './services/category.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
-  constructor() { }
+  categories: Array<Taxon> = [];
+  constructor(
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit() {
+    this.categoryService.getCategories()
+      .subscribe( res => this.categories = res );
   }
 
 }
