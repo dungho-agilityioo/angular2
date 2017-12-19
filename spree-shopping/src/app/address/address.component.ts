35414ab7a27c099d3c6fa4fa3f74ea3d433c8841
DungHo <dung.hoduy@asnet.com.vn>
2017-12-19 09:19:54 +0700

    [#14] Just assign input into cart summary component, not use code inside that component

--- spree-shopping/src/app/address/address.component.ts
+++ spree-shopping/src/app/address/address.component.ts
@@ -1,7 +1,10 @@
+
 import {
   Component,
   OnInit,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnDestroy
 } from '@angular/core';
 import {
   FormBuilder,
@@ -11,9 +14,13 @@ import {
   AbstractControl
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import * as _ from 'lodash';
 
 import { OrderService } from 'app/order/services/order.service';
 import { AddressService } from './services/address.service';
+import { Address } from 'app/address/models/address';
 
 @Component({
   selector: 'address-form',
@@ -21,37 +28,60 @@ import { AddressService } from './services/address.service';
   styleUrls: ['./address.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddressComponent implements OnInit {
+export class AddressComponent implements OnInit, OnDestroy {
   addressForm: FormGroup;
   emailForm: FormGroup;
   isAuthenticated: boolean;
+  subscription: Subscription;
+  orderState: String;
+  address: Address;
+  email: String;
 
   constructor(
     private formBuilder: FormBuilder,
     private orderService: OrderService,
     private addressService: AddressService,
-    private router: Router
+    private router: Router,
+    private cd: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
-    this.addressForm = this.addressService.initAddressForm();
-    this.emailForm = this.addressService.initEmailForm();
+    this.orderService.order$.subscribe(res => {
+      if (!_.isEmpty(res)) {
+        const order = res.json();
+        this.addressForm = this.addressService.initAddressForm();
+        this.emailForm = this.addressService.initEmailForm();
+        this.orderState = order.state;
+        this.address = order.ship_address;
+        this.email = order.email;
+        this.cd.markForCheck();
+      }
+    });
     // handle after
     this.isAuthenticated = false;
   }
 
   saveAddress(address) {
     let addressAttributes;
-    if ( this.isAuthenticated) {
+    if (this.isAuthenticated) {
       addressAttributes = this.addressService.createAddresAttributes(address);
     } else {
       const email = this.getEmailFromUser();
       addressAttributes = this.addressService.createGuestAddressAttributes(address, email);
     }
-    this.orderService.updateOrder(addressAttributes)
-      .subscribe();
-
-    this.router.navigate(['checkout/delivery']);
+console.log('state in address ', this.orderState);
+    if (this.orderState === 'delivery') {
+      this.subscription = this.orderService.updateOrder(addressAttributes)
+        .subscribe(
+          success => this.router.navigate(['checkout/delivery']),
+          error => {
+            console.log('errr', error);
+            this.router.navigate(['checkout/address']);
+          }
+        );
+    } else {
+      this.router.navigate(['checkout/delivery']);
+    }
   }
 
   /**
@@ -61,4 +91,9 @@ export class AddressComponent implements OnInit {
     return this.emailForm.value.email;
   }
 
+  ngOnDestroy() {
+    if (!_.isUndefined(this.subscription)) {
+      // this.subscription.unsubscribe();
+    }
+  }
 }
--- spree-shopping/src/app/cart/components/cart-address/cart-address.component.ts
+++ spree-shopping/src/app/cart/components/cart-address/cart-address.component.ts
@@ -1,7 +1,16 @@
+import { Subscription } from 'rxjs/Subscription';
 import {
   Component,
   OnInit,
-  ChangeDetectionStrategy } from '@angular/core';
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  OnDestroy
+} from '@angular/core';
+
+import * as _ from 'lodash';
+
+import { OrderService } from 'app/order/services/order.service';
+import { OrderTotal } from 'app/order/models/order-total.model';
 
 @Component({
   selector: 'app-cart-address',
@@ -9,11 +18,32 @@ import {
   styleUrls: ['./cart-address.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CartAddressComponent implements OnInit {
+export class CartAddressComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  orderTotal: OrderTotal;
 
-  constructor() { }
+  constructor(
+    private orderService: OrderService,
+    private cd: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.orderService.order$.subscribe(res => {
+      if (!_.isEmpty(res)) {
+        const order = res.json();
+        this.orderTotal = {
+          itemTotal: order.item_total,
+          total: order.total,
+          itemCount: order.total_quantity,
+          taxTotal: order.tax_total
+        };
+        this.cd.markForCheck();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
