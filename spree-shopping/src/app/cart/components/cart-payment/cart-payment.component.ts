271ccf8ed8406681e0eeeb62b57c1d60c972c557
DungHo <dung.hoduy@asnet.com.vn>
2017-12-19 14:53:43 +0700

    [#14] Display payment info during checkout

---
+++ spree-shopping/src/app/cart/components/cart-payment/cart-payment.component.ts
@@ -0,0 +1,46 @@
+import { Subscription } from 'rxjs/Subscription';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import * as _ from 'lodash';
+
+import { OrderService } from 'app/order/services/order.service';
+import { OrderTotal } from 'app/order/models/order-total.model';
+import { PaymentService } from 'app/payment/services/payment.service';
+
+@Component({
+  selector: 'app-cart-payment',
+  templateUrl: './cart-payment.component.html',
+  styleUrls: ['./cart-payment.component.css'],
+  providers: [PaymentService]
+})
+export class CartPaymentComponent implements OnInit {
+  subscription: Subscription;
+  orderTotal: OrderTotal;
+  constructor(
+    private orderService: OrderService,
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    this.subscription = this.orderService.order$.subscribe( res => {
+      if (!_.isEmpty(res)) {
+        const order = res.json();
+
+        this.orderTotal = {
+          number: order.number,
+          itemTotal: order.item_total,
+          total: order.total,
+          itemCount: order.total_quantity,
+          taxTotal: order.tax_total
+        };
+
+        this.cd.markForCheck();
+      }
+    });
+  }
+
+}
