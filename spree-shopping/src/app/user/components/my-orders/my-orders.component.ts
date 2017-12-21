6b97ca7f5cb56ff2c7691c926934e7d7a9236c29
DungHo <dung.hoduy@asnet.com.vn>
2017-12-21 17:44:46 +0700

    [#16] Display my order

---
+++ spree-shopping/src/app/user/components/my-orders/my-orders.component.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import { UserService } from 'app/user/services/user.service';
+import { Order } from 'app/order/models/order.model';
+import { LocalStorageService } from 'app/core/services/local-storage.service';
+
+@Component({
+  selector: 'app-my-orders',
+  templateUrl: './my-orders.component.html',
+  styleUrls: ['./my-orders.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MyOrdersComponent implements OnInit {
+  orders: any;
+  constructor(
+    private userService: UserService,
+    private localStorageService: LocalStorageService,
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    const email = this.localStorageService.getUser().email;
+    this.userService.getOrders(email)
+      .subscribe( res => {
+        // const data = res.json();
+        this.orders = [...res.orders];
+        this.cd.markForCheck();
+        console.log('orders 11', this.orders);
+      });
+
+  }
+
+}
--- spree-shopping/src/app/user/user.module.ts
+++ spree-shopping/src/app/user/user.module.ts
@@ -1,19 +1,32 @@
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import {
   NgModule
 } from '@angular/core';
-import {
-  UserComponent
-} from './user.component';
+
+import { UserService } from './services/user.service';
+import { OrderModule } from 'app/order/order.module';
+
+const MODULES = [
+  OrderModule
+];
 
 const COMPONENTS = [
-  UserComponent
+  MyOrdersComponent
+];
+
+const PROVIDERS = [
+  UserService
 ];
 
 @NgModule({
   imports: [
+    ...MODULES
   ],
   declarations: [
     ...COMPONENTS
+  ],
+  providers: [
+    ...PROVIDERS
   ]
 })
 export class UserModule { }
