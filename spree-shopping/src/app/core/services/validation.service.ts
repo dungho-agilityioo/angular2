6bd8de0ac581f98407ec532ca01e4f9cfc7f606b
DungHo <dung.hoduy@asnet.com.vn>
2017-12-25 14:27:11 +0700

    [#25] Create message control component to show error message

---
+++ spree-shopping/src/app/core/services/validation.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+import * as _ from 'lodash';
+
+import * as coreConfig from 'app/core/core-config';
+
+@Injectable()
+export class ValidationService {
+
+  static getValidatorErrorMessage(validatorName: string, validatorValue?: any, controlName?: string) {
+    const messageConfig = coreConfig.VALIDATION.MESSAGES;
+
+    const complied = _.template(messageConfig[validatorName]);
+
+    return complied({
+      name: controlName
+    });
+  }
+
+
+  static emailValidator(control: AbstractControl) {
+    // RFC 2822 compliant regex
+    // tslint:disable-next-line:max-line-length
+    if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+      return null;
+    } else {
+      return { 'invalidEmailAddress': true };
+    }
+  }
+
+  static passwordValidator(control) {
+    // {6,100}           - Assert password is between 6 and 100 characters
+    // (?=.*[0-9])       - Assert a string has at least one number
+    if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+      return null;
+    } else {
+      return { 'invalidPassword': true };
+    }
+  }
+
+  static phoneValidator() {
+
+    return (control: AbstractControl): { [key: string]: any } => {
+      const value = control.value;
+
+      if ( _.isUndefined(value) || value === '' ) {
+        return null;
+      }
+
+      const phone = value.match(/\d/g);
+
+      if ( !_.isNull(phone) && (_.isNull(phone) || phone.length === 10 || phone.length === 11)) {
+        return null;
+      } else {
+        return { 'invalidPhone': true };
+      }
+    };
+  }
+
+  static passwrodMatch(control: AbstractControl): { [key: string]: boolean } {
+    const password = control.get('password');
+    const passwordConfirm = control.get('passwordConfirmation');
+
+    if ( !password || !passwordConfirm ) { return null; }
+    return password.value === passwordConfirm.value ? null : { nomatch: true };
+  }
+
+}
