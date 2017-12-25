b3c006a6c8ab1984339b3642822c69aafd782ba1
DungHo <dung.hoduy@asnet.com.vn>
2017-12-25 17:57:23 +0700

    [#9] Create image component

---
+++ spree-shopping/src/app/product/components/product-images/product-images.component.ts
@@ -0,0 +1,36 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import * as _ from 'lodash';
+
+import { Image } from 'app/product/models/image.model';
+
+@Component({
+  selector: 'product-images',
+  templateUrl: './product-images.component.html',
+  styleUrls: ['./product-images.component.scss']
+})
+export class ProductImagesComponent implements OnInit {
+  @Input() images: Array<Image>;
+  currentImage: Image = new Image();
+
+  constructor(
+    private cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    if (!_.isNull(this.images)) {
+      this.currentImage = this.images[0];
+    }
+  }
+
+  selectedImage(image: Image): void {
+    this.currentImage = _.assignIn({}, image) ;
+    this.cd.markForCheck();
+  }
+
+}
