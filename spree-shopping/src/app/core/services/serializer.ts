93950b00f5259f1862d04558732a05c73555e03c
DungHo <dung.hoduy@asnet.com.vn>
2017-12-27 22:04:19 +0700

    [#26] Fix function convert key of object nested to camelCase

--- spree-shopping/src/app/core/services/serializer.ts
+++ spree-shopping/src/app/core/services/serializer.ts
@@ -1,7 +1,6 @@
 import * as coreConfig from 'app/core/core-config';
 
 export class Serializer {
-  static rs = {};
 
   static deserialize(objects: any) {
     const rs = {};
@@ -11,10 +10,25 @@ export class Serializer {
       if (objects.hasOwnProperty(key)) {
         value = objects[key];
         if (typeof objects[key] === 'object') {
-          value = this.deserialize(objects[key]);
+          let isObject = true;
+          const arrValues = [];
+          // if value is array
+          if (value instanceof Array) {
+            isObject = false;
+            // each array to convert field name
+            value.forEach((item, index) => {
+              const val = this.deserialize(item);
+              arrValues.push(val);
+            });
+          } else {
+            value = this.deserialize(value);
+          }
+          fieldName = this.getFieldName(key);
+          rs[fieldName] = isObject ? value : arrValues;
+        } else {
+          fieldName = this.getFieldName(key);
+          rs[fieldName] = value;
         }
-        fieldName = coreConfig.MAP_FIELDS[key] || this.convertCamelCased(key);
-        rs[fieldName] = value;
       }
     }
     return rs;
@@ -25,6 +39,10 @@ export class Serializer {
                                                         return g[1].toUpperCase();
                                                       });
     return camelCased;
+  }
+
+  private static getFieldName(key: string): string {
+    return coreConfig.MAP_FIELDS[key] || this.convertCamelCased(key);
   }
 
 }
